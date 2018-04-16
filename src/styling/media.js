d5f4a24a49eeee30f635861187dc3bdc4037d622
Prakhar Keshari <prakhar.odyssey@gmail.com>
2018-04-16 15:31:06 -0500

    media queries added with styled components

--- src/styling/media.js
+++ src/styling/media.js
@@ -1,20 +1,51 @@
-// import {css} from 'styled-components'
+import {css} from 'styled-components'
 
-// const sizes = {
-//     phone: 376,
-//     tablet: 768,
-//     desktop: 992,
-//     giant: 1170 
+const sizes = {
+    phone: 376,
+    tablet: 768,
+    desktop: 1500
+}
+
+const media = Object.keys(sizes).reduce((finalMedia, size) => {
+    return{
+        ...finalMedia,
+        [size]: function(...args) {
+            return css`
+                @media(max-width: ${sizes[size]}px){
+                ${css(...args)}
+                }
+            `
+        }
+    }
+}, {})
+
+// function phone(...args) {
+//     return css`
+//         @media(max-width: ${sizes.phone}px){
+//             ${css(...args)}
+//         }
+//     `
 // }
 
+// function tablet(...args) {
+//     return css`
+//         @media(max-width: ${sizes.tablet}px){
+//             ${css(...args)}
+//         }
+//     `
+// }
 
+// function desktop(...args) {
+//     return css`
+//         @media(max-width: ${sizes.desktop}px){
+//             ${css(...args)}
+//         }
+//     `
+// }
 
 // const media = {
+//     phone,
+//     tablet,
 //     desktop
 // }
-
-// const wrapper = styled.div`
-//     padding = 0 30%;
-// `
-
-// export default media
+export default media

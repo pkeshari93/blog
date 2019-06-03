86e528139970e7fa6e8eacfe80d2356854407193
Prakhar Keshari <prakhar.odyssey@gmail.com>
2019-06-03 07:33:39 +0530

    style fixes to social media links

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react'
-import styled from 'styled-components'
-import media from '../styling/media'
-var FontAwesome = require('react-fontawesome');
-
+import React, { Component } from "react";
+import styled from "styled-components";
+import media from "../styling/media";
+var FontAwesome = require("react-fontawesome");
 
 const Container = styled.div`
     ${media.desktop`
-        display: block;
+        display: flex;
+        flex-direction: column;
         text-align: center;
         width: 225px;
         height: auto;
@@ -26,7 +26,7 @@ const Container = styled.div`
             font-size: 35px
             &:hover{     
                 padding-left: 30px;
-                padding-right: 5px;
+                padding-right: 0px;
                 background: #000;
                 border-radius: 5px;
                 color: #fff;
@@ -41,14 +41,10 @@ const Container = styled.div`
         left: 10px;
         right: 0;
     
-    `
-        
-	
-	}
+    `}
     ${media.tablet`
         display: block;
         text-align: center;
-        width: 225px;
         height: auto;
         background: #fff;
         border-radius: 10px;
@@ -65,15 +61,13 @@ const Container = styled.div`
             font-size: 25px
         }
         padding: 2px 10px;        
-        width: 52px;
         justify-content: space-around;
         position: fixed;
         top: 100px;
         left: 10px;
         right: 0;
         
-	`
-	}
+	`}
     ${media.phone`
         display: block;
         text-align: center;
@@ -94,30 +88,70 @@ const Container = styled.div`
             font-size: 25px
             padding: 15px;
         }  
-	`
-    }
-`
+	`}
+`;
 
-const Social = styled.li`
-    padding: 2px;
-    margin: 10px 0;
-    color: #333;
-    display: inline-block;
-    text-decoration: none;
-`
+const Social = styled.div`
+  display: block;
+  width: 50px;
+  margin: 10px 0;
+  color: #333;
+  display: inline-block;
+  text-decoration: none;
+`;
 
-export default class SocialMedia extends Component{
-    render(){
-        return(
-            <Container>
-                {/* <Connect>Connect with me</Connect> */}
-                <ul>
-                    <Social><a target="_blank" href="https://www.linkedin.com/in/prakhar-keshari-68163b84/"><i className="fab fa-linkedin-in"></i></a></Social>
-                    <Social><a target="_blank" href="https://github.com/pkeshari93"><i className="fab fa-github-square"></i></a></Social>
-                    <Social><a target="_blank" href="https://www.instagram.com/prakhar.odyssey/"><i className="fab fa-instagram"></i></a></Social>
-                    <Social><a target="_blank" href="https://www.facebook.com/keshari.prakhar"><i className="fab fa-facebook-square"></i></a></Social>  
-                </ul>                                              
-            </Container>
-        )
-    }
+const SocialMediaLinks = styled.a`
+  color: #313740;
+  padding: 0 15px;
+  font-size: 35px;
+  &:hover {
+    padding-left: 40px;
+    padding-right: 5px;
+    background: #000;
+    border-radius: 5px;
+    color: #fff;
+    transition: 0.2s;
+  }
+`;
+
+export default class SocialMedia extends Component {
+  render() {
+    return (
+      <Container>
+        {/* <Connect>Connect with me</Connect> */}
+        <Social>
+          <SocialMediaLinks
+            target="_blank"
+            href="https://www.linkedin.com/in/prakhar-keshari-68163b84/"
+          >
+            <i className="fab fa-linkedin-in" />
+          </SocialMediaLinks>
+        </Social>
+        <Social>
+          <SocialMediaLinks
+            target="_blank"
+            href="https://github.com/pkeshari93"
+          >
+            <i className="fab fa-github-square" />
+          </SocialMediaLinks>
+        </Social>
+        <Social>
+          <SocialMediaLinks
+            target="_blank"
+            href="https://www.instagram.com/prakhar.odyssey/"
+          >
+            <i className="fab fa-instagram" />
+          </SocialMediaLinks>
+        </Social>
+        <Social>
+          <SocialMediaLinks
+            target="_blank"
+            href="https://www.facebook.com/keshari.prakhar"
+          >
+            <i className="fab fa-facebook-square" />
+          </SocialMediaLinks>
+        </Social>
+      </Container>
+    );
+  }
 }
--- src/styling/media.js
+++ src/styling/media.js
@@ -1,51 +1,22 @@
-import {css} from 'styled-components'
+import { css } from "styled-components";
 
 const sizes = {
-    phone: 376,
-    tablet: 768,
-    desktop: 1500
-}
+  phone: 376,
+  tablet: 768,
+  desktop: 1500
+};
 
 const media = Object.keys(sizes).reduce((finalMedia, size) => {
-    return{
-        ...finalMedia,
-        [size]: function(...args) {
-            return css`
-                @media(max-width: ${sizes[size]}px){
-                ${css(...args)}
-                }
-            `
+  return {
+    ...finalMedia,
+    [size]: function(...args) {
+      return css`
+        @media (max-width: ${sizes[size]}px) {
+          ${css(...args)}
         }
+      `;
     }
-}, {})
+  };
+}, {});
 
-// function phone(...args) {
-//     return css`
-//         @media(max-width: ${sizes.phone}px){
-//             ${css(...args)}
-//         }
-//     `
-// }
-
-// function tablet(...args) {
-//     return css`
-//         @media(max-width: ${sizes.tablet}px){
-//             ${css(...args)}
-//         }
-//     `
-// }
-
-// function desktop(...args) {
-//     return css`
-//         @media(max-width: ${sizes.desktop}px){
-//             ${css(...args)}
-//         }
-//     `
-// }
-
-// const media = {
-//     phone,
-//     tablet,
-//     desktop
-// }
-export default media
+export default media;
